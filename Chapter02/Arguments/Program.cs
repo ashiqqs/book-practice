77d496c41588c01f939b2205972c112ccccf8a60
ashiqqa <ashiqmohammadshopon@gmail.com>
2020-12-06 22:32:01 +0600

    number type range, main method args

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using static System.Console;
+namespace Arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"There are {args.Length} Arguments");
+            ForegroundColor = (ConsoleColor)Enum.Parse(enumType:typeof(ConsoleColor), value:args[0], ignoreCase:true);
+            BackgroundColor = (ConsoleColor)Enum.Parse(enumType:typeof(ConsoleColor),value:args[1], ignoreCase:true);
+            WindowWidth = int.Parse(args[3]);
+            WindowHeight = int.Parse(args[2]);
+            
+        }
+    }
+}
