39990377315054d66d8ad0cfc117ce3caacfb20c
ashiqqa <ashiq.mail.net@gmail.com>
2020-12-06 22:38:22 +0600

    enum, tuples

---
+++ Chapter05/EnumType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+namespace EnumType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ashiq = new Person();
+            ashiq.Name = "Ashiq";
+            ashiq.Hobby = Hobby.Traveling | Hobby.Gardening;
+
+            WriteLine($"{ashiq.Name} likes to do {ashiq.Hobby} ");
+        }
+    }
+    public class Person{
+        public string Name { get; set; }
+        public Hobby Hobby{get;set;}
+    }
+    [System.Flags]
+    public enum Hobby{
+        Reading = 1,
+        Gardening = 2,
+        Traveling = 3
+    }
+}
---
+++ Chapter05/Tuples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tuples
+{
+    class Program
+    {
+        public string Name=>"Ashiq";
+        public static  (string Name, int Price) GetItems(){
+            return (Name: "Apples", Price:150);
+        }
+        public Program this[int index]{
+            get{
+                return Name;
+            }
+            set{
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            (string Name, int Price) item = GetItems();
+            var c = ("ashiq",5);
+            Console.WriteLine($"Name:{c.Item1} Price: {c.Item2}");
+        }
+    }
+}
