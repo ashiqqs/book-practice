785800b6862062a1f8070368d15062801a598838
ashiqqa <ashiq.mail.net@gmail.com>
2021-01-16 11:32:31 +0600

    packaging

---
+++ Chapter07/Packaging/StringValidator/Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using StringValidator;
+namespace Test
+{
+    public class Class1
+    {
+        public void Test(string input)
+        {
+            input.IsValidPassword();
+        }
+    }
+}
